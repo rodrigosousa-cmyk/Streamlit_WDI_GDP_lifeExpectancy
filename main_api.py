ef76bb5b89d5b2904fc3353bdd464a617c6d0cbb
rodrigosousa-cmyk <rodrigo.sousa@estudante.ufjf.br>
2025-11-26 14:39:12 -0300

    Update main_api.py

--- main_api.py
+++ main_api.py
@@ -21,7 +21,7 @@ def fn_scatterplot(df):
         range_y=[df.lifeExp.min(),df.lifeExp.max()+10],
         # title='Emissions vs Cumulative Emissions Over Time'
     )
-    fig.update_layout(height=600, margin=dict(l=0, r=0, t=20, b=0))
+    fig.update_layout(height=550, margin=dict(l=0, r=0, t=20, b=0))
     return fig
 # Function Create choropleth map
 def create_choropleth(df,indicator):
@@ -46,7 +46,7 @@ def fn_sunburst(data,year):
                     color_continuous_scale='RdBu',
                     # title=f'Life Expectancy vs GDP PerCapta ({year})'
                      )
-    fig.update_layout(height=450, margin=dict(l=0, r=0, t=0, b=0))
+    fig.update_layout(height=550, margin=dict(l=0, r=0, t=0, b=0))
     return fig
 # end of functions
 # Set page config
@@ -145,6 +145,7 @@ with tab4:
     )
     st.plotly_chart(fn_sunburst(df, selected_year), use_container_width=True)
 # END #
+
 
 
 
